ef86a9c3155be84f8d8fcf733fc0b484102ea98a
Mapkov2 <akaorcram21@gmail.com>
2026-02-17 09:36:34 +0100

    preparing last 2.0 beta version

--- MidnightSimpleUnitFrames/MidnightSimpleUnitFrames.lua
+++ MidnightSimpleUnitFrames/MidnightSimpleUnitFrames.lua
@@ -4858,7 +4858,7 @@ end
     if type(ns.Castbars._InitPlayerCastbarPreviewToggle) == "function" then
         C_Timer.After(1.1, ns.Castbars._InitPlayerCastbarPreviewToggle)
     end
-    print("|cff7aa2f7MSUF|r: |cffc0caf5/msuf|r |cff565f89to open options|r  |cff565f89|r  |cff9ece6a Beta Build 2.0 Beta 9 |cff565f89|r  |cffc0caf5 Thank you for using MSUF -|r  |cfff7768eReport bugs in the Discord.|r")
+    print("|cff7aa2f7MSUF|r: |cffc0caf5/msuf|r |cff565f89to open options|r  |cff565f89|r  |cff9ece6a Beta Build 2.0 Beta 10 |cff565f89|r  |cffc0caf5 Thank you for using MSUF -|r  |cfff7768eReport bugs in the Discord.|r")
  end, nil, true)
 do
     if not _G.MSUF__BucketUpdateManager then
